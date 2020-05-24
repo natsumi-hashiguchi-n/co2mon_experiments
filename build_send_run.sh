7c4a02b05f61b1f79892e20240f596b017969fd5
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2020-05-25 03:47:07 +0900

    アプリケーションを起動するservice unitを追加した

--- build_send_run.sh
+++ build_send_run.sh
@@ -5,7 +5,9 @@
 #   brew install pv
 #
 
-TARGET_HOST="cm01.local"
+set -eu
+
+TARGET_HOST="${1}"
 CONTAINER_NAME="co2mon"
 
 pv --version > /dev/null || (echo "pvが見当たりません" >&2; exit 1)
@@ -15,5 +17,6 @@ pv --version > /dev/null || (echo "pvが見当たりません" >&2; exit 1)
     pv |
     ssh "${TARGET_HOST}" docker image load) &&
   ssh "${TARGET_HOST}" '
+    sudo systemctl stop co2mon.service;
     docker stop '"${CONTAINER_NAME}"';
     docker run -d --privileged --rm -v /var/local/co2mon:/var/local/co2mon --name '"${CONTAINER_NAME}"' co2mon /sbin/init'
