178d6f1cf0e57b4511deb99d105b1fb9a35a3e36
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2020-05-27 00:13:30 +0900

    ディレクトリではなくディスクそのものが存在するかどうかを確認するようにした

--- write_sd.sh
+++ write_sd.sh
@@ -4,12 +4,11 @@ image="${HOME}/Downloads/2020-02-13-raspbian-buster-lite.img"
 
 sudo printf ''  # sudoをいちどキックしておく
 
-while :; do
-  echo "waiting /Volumes/boot ..."
-  test -d /Volumes/boot && break
+while ! diskutil info /dev/disk2 > /dev/null; do
+  echo "waiting /dev/disk2 ..."
   sleep 1
 done
-echo "/Volumes/boot found!"
+echo "/dev/disk2 found!"
 
 diskutil unmountDisk /dev/disk2
 
